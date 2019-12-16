92a3649f3fd7716d738ef0d5eaff50799eeb2dae
mjz-smu <mzaber@smu.edu>
2019-12-16 10:32:00 -0600

    more mulpy functions

--- src/py_mulval/mulpy_mitre_tools.py
+++ src/py_mulval/mulpy_mitre_tools.py
@@ -80,16 +80,23 @@ class mitre_cve(object):
     else:
       logging.debug('cve_raw already exists: %s' % CVE_LOCAL_FILE_RAW)
 
-  def convert_cves_to_json(self):
+  def write_cves_to_json(self, overwrite_if_exists=False):
     cve_raw = pathlib.Path(CVE_LOCAL_FILE_NAME_RAW)
     cve_json = pathlib.Path(CVE_LOCAL_FILE_PROCESSED)
-    if not cve_json.exists():
+    if not cve_json.exists() or overwrite_if_exists:
       logging.debug('writing cve_json: %s' % CVE_LOCAL_FILE_PROCESSED)
       cve_df = pd.read_csv(cve_raw, sep=',', quotechar='"', header=2,
                            skiprows=range(3, 10), encoding="ISO-8859-1")
-      cve_df.to_csv(CVE_LOCAL_FILE_PROCESSED)
+      cve_df.to_json(CVE_LOCAL_FILE_PROCESSED, orient='table')
     else:
       logging.debug('cve_json already exists: %s' % CVE_LOCAL_FILE_PROCESSED)
+
+  def read_cves_from_json(self, *args, **kwargs):
+    date_cols = ['publishedDate', 'lastModifiedDate']
+
+    # return pd.read_json(CVE_LOCAL_FILE_PROCESSED, convert_dates=date_cols,
+    #                     orient='table')
+    return pd.read_json(CVE_LOCAL_FILE_PROCESSED, orient='table')
 
 
 class nist_nvd(object):
@@ -118,11 +125,44 @@ class nist_nvd(object):
     logging.debug(self.nvd_file_list)
 
   def write_nvd_to_json(self, overwrite_if_exists=False):
-    json_dfs = []
     outfile = pathlib.Path(NVD_LOCAL_FILE)
     cve_dfs = []
-    # cve_df = pd.DataFrame()
-    if not outfile.exists() and not overwrite_if_exists:
+    date_cols = ['publishedDate',
+                 'lastModifiedDate']
+    bool_cols = ['impact.baseMetricV2.acInsufInfo',
+                 'impact.baseMetricV2.obtainAllPrivilege',
+                 'impact.baseMetricV2.obtainUserPrivilege',
+                 'impact.baseMetricV2.obtainOtherPrivilege',
+                 'impact.baseMetricV2.userInteractionRequired']
+    category_cols = [
+      'cve.data_type',
+      'cve.data_format',
+      'cve.data_version',
+      'cve.CVE_data_meta.ASSIGNER',
+      'configurations.CVE_data_version',
+      'impact.baseMetricV3.cvssV3.version',
+      #        'impact.baseMetricV3.cvssV3.vectorString',
+      'impact.baseMetricV3.cvssV3.attackVector',
+      'impact.baseMetricV3.cvssV3.attackComplexity',
+      'impact.baseMetricV3.cvssV3.privilegesRequired',
+      'impact.baseMetricV3.cvssV3.userInteraction',
+      'impact.baseMetricV3.cvssV3.scope',
+      'impact.baseMetricV3.cvssV3.confidentialityImpact',
+      'impact.baseMetricV3.cvssV3.integrityImpact',
+      'impact.baseMetricV3.cvssV3.availabilityImpact',
+      'impact.baseMetricV3.cvssV3.baseSeverity',
+      'impact.baseMetricV2.cvssV2.version',
+      #        'impact.baseMetricV2.cvssV2.vectorString',
+      'impact.baseMetricV2.cvssV2.accessVector',
+      'impact.baseMetricV2.cvssV2.accessComplexity',
+      'impact.baseMetricV2.cvssV2.authentication',
+      'impact.baseMetricV2.cvssV2.confidentialityImpact',
+      'impact.baseMetricV2.cvssV2.integrityImpact',
+      'impact.baseMetricV2.cvssV2.availabilityImpact',
+      'impact.baseMetricV2.severity',
+    ]
+
+    if not outfile.exists() or overwrite_if_exists:
       logging.debug('writing nist_nvd to json: %s...' % NVD_LOCAL_FILE)
       for gzip_file in self.nvd_file_list:
         year_df = pd.read_json(SEP.join((DOWNLOAD_DIR, gzip_file)))
@@ -131,13 +171,21 @@ class nist_nvd(object):
       for col in cve_df.columns:
         print(col)
       cve_df = cve_df.set_index(['cve.CVE_data_meta.ID'])
-      cve_df.to_json(NVD_LOCAL_FILE)
+      cve_df[bool_cols] = cve_df[bool_cols].astype(bool)
+      cve_df[category_cols] = cve_df[category_cols].astype('category')
+      # 'table="orient" can not yet read timezone ' "data")
+      # cve_df[date_cols] = cve_df[date_cols].apply(pd.to_datetime)
+      print(cve_df.info())
+      cve_df.to_json(NVD_LOCAL_FILE, orient='table')
     else:
       logging.debug('found nvd_nist file at: %s' % NVD_LOCAL_FILE)
 
   def read_nvd_from_json(self, *args, **kwargs):
     date_cols = ['publishedDate', 'lastModifiedDate']
-    return pd.read_json(NVD_LOCAL_FILE, convert_dates=date_cols)
+
+    # raise NotImplementedError('table="orient" can not yet read timezone ' "data")
+    return pd.read_json(NVD_LOCAL_FILE, convert_dates=date_cols, orient='table')
+    # return pd.read_json(NVD_LOCAL_FILE, orient='table')
 
 
 def setup():
@@ -171,11 +219,64 @@ if __name__ == "__main__":
 
   cve = mitre_cve()
   cve.get_cves_from_mitre()
-  cve.convert_cves_to_json()
+  cve.write_cves_to_json()
+  # cve.write_cves_to_json(overwrite_if_exists=True)
+  cve_df = cve.read_cves_from_json()
+  print(cve_df.describe())
+  print(cve_df.info())
+  print(cve_df.head())
 
   nvd = nist_nvd()
   nvd.get_nvd_from_nist()
+  # nvd.write_nvd_to_json(overwrite_if_exists=True)
   nvd.write_nvd_to_json()
   nvd_df = nvd.read_nvd_from_json()
-  nvd_df.describe()
 
+  v2_cols = {'impact.baseMetricV2.cvssV2.version': 'v2.version',
+             'impact.baseMetricV2.cvssV2.vectorString': 'v2.vectorString',
+             'impact.baseMetricV2.cvssV2.accessVector': 'v2.accessVector',
+             'impact.baseMetricV2.cvssV2.accessComplexity': 'v2.accessComplexity',
+             'impact.baseMetricV2.cvssV2.authentication': 'v2.authentication',
+             'impact.baseMetricV2.cvssV2.confidentialityImpact': 'v2.confidentialityImpact',
+             'impact.baseMetricV2.cvssV2.integrityImpact': 'v2.integrityImpact',
+             'impact.baseMetricV2.cvssV2.availabilityImpact': 'v2.availabilityImpact',
+             'impact.baseMetricV2.cvssV2.baseScore': 'v2.baseScore',
+             'impact.baseMetricV2.severity': 'v2.severity',
+             'impact.baseMetricV2.exploitabilityScore': 'v2,exploitabilityScore',
+             'impact.baseMetricV2.impactScore': 'v2.impactScore',
+             'impact.baseMetricV2.acInsufInfo': 'v2.acInsufInfo',
+             'impact.baseMetricV2.obtainAllPrivilege': 'v2.obtainAllPrivilege',
+             'impact.baseMetricV2.obtainUserPrivilege': 'v2.obtainUserPrivilege',
+             'impact.baseMetricV2.obtainOtherPrivilege': 'v2.obtainOtherPrivilege',
+             'impact.baseMetricV2.userInteractionRequired': 'v2.userInteractionRequired`'}
+
+  v3_cols = {'impact.baseMetricV3.cvssV3.version': 'v3.version',
+             'impact.baseMetricV3.cvssV3.vectorString': 'v3.vectorString',
+             'impact.baseMetricV3.cvssV3.attackVector': 'v3.attackVector',
+             'impact.baseMetricV3.cvssV3.attackComplexity': 'v3.attackComplexity',
+             'impact.baseMetricV3.cvssV3.privilegesRequired': 'v3.privilegesRequired',
+             'impact.baseMetricV3.cvssV3.userInteraction': 'v3.userInteraction',
+             'impact.baseMetricV3.cvssV3.scope': 'v3.scope',
+             'impact.baseMetricV3.cvssV3.confidentialityImpact': 'v3.confidentialityImpact',
+             'impact.baseMetricV3.cvssV3.integrityImpact': 'v3.integrityImpact',
+             'impact.baseMetricV3.cvssV3.availabilityImpact': 'v3.availabilityImpact',
+             'impact.baseMetricV3.cvssV3.baseScore': 'v3.baseScore',
+             'impact.baseMetricV3.cvssV3.baseSeverity': 'v3.baseSeverity',
+             'impact.baseMetricV3.exploitabilityScore': 'v3.exploitabilityScore',
+             'impact.baseMetricV3.impactScore': 'v3.impactScore', }
+
+  common_cols = ['publishedDate', 'lastModifiedDate', 'cve.data_type',
+                 'cve.data_format',
+                 'cve.data_version', 'cve.CVE_data_meta.ASSIGNER',
+                 'cve.problemtype.problemtype_data',
+                 'cve.references.reference_data',
+                 'cve.description.description_data',
+                 'configurations.CVE_data_version',
+                 'configurations.nodes', ]
+
+  # make easy names
+  nvd_df = nvd_df.rename(columns={**v2_cols, **v3_cols})
+
+  print(nvd_df.describe())
+  print(nvd_df.info())
+  print(nvd_df.head())
